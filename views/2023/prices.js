890a4652a25e2d4a6d39ed1cbba9f2c16dca9b9b
Gerald Wodni <gerald.wodni@gmail.com>
2023-06-17 01:22:17 +0200

    EuroForth 2023: templates added

--- views/2023/prices.js
+++ views/2023/prices.js
@@ -7,9 +7,32 @@ module.exports = {
     myName: "myEuroForth",
     operatorEmail: "gerald.wodni@gmail.com",
     operatorBankAccount: "HINT: provided via environment variable",
-    chatRegistration: "HINT: provided via environment variable",
+
+    /* message to show after successful registration */
+    successTemplate: function _successTemplate( { price } ) {
+        return `Thank you for your registration!`
+            + `\nPlease transfer €${price} to the bank account provided in your email.`
+            //- + `\nIn the email you will find a personalized 'myEuroForth'-Link. Please bookmark it right away,`
+            //- + `\nas this link allows you to participate electronically and to edit your presentation details.`
+            + `\nYou will receive the email from: ${process.env.SMTP_EMAIL}.`;
+    },
+    /* registration email text body */
+    emailTemplate: function _emailTemplate( { prices, values, price, website } ) {
+        return `Hotel: ${values.hotel}\nExtra days: ${values.extraDays}\nPreis (total): ${price}`
+            + ( price > 0 ? `\nPlease transfer the full amount in Euro to:\n${process.env.OPERATOR_BANK_ACCOUNT.replace(/\\n/g, '\n')}` : '' )
+            + ( prices.chatRegistration ? `\n\n${process.env.CHAT_REGISTRATION}` : '' )
+            //- + `\n\nTo attend the conference electronically, and to edit your presentation details, please use the following link: https://${website}/${prices.myName}/${values.editHash}`
+            + `\n\nName: ${values.name}`
+            + `\nAddress: ${values.address}`
+            + `\nTelephone: ${values.telephone}`
+            + `\nEmail: ${values.email}`
+            + `\n\nEntourage: ${values.partner}\nName: ${values.partnerName}\nAdresse: ${values.partnerAddress}`
+            + `\n\nPresentation: ${values.presentationTitle} Length: ${values.presentationLength}\n${values.presentationDescription}`
+            + `\n\nRemark: ${values.remark}`;
+    },
     meeting: {
         openRegistration: "2023-06-15",
+        enablePresentation: true,
         enablePartner: true,
         start: "2023-09-02",
     },
