2378c217ce8137c1677f3ea3ca20766653a4ff0c
Gerald Wodni <gerald.wodni@gmail.com>
2023-06-22 09:34:30 +0200

    adjusted the pricing according to accountant

--- views/2023/prices.js
+++ views/2023/prices.js
@@ -38,35 +38,19 @@ module.exports = {
     },
     hotels: [
         {
-            header: "Forth standard meeting &  EuroForth conference (13.-17. September) with VAT",
-            description: "Choose this if you are a private traveller in the EU and cannot provide a VAT-ID, or if you live in Austria",
+            header: "Forth standard meeting &  EuroForth conference (13.-17. September)",
+            description: "Hotel with Breakfest, Meeting including Lunch",
             modes: [
-                { name: "Single room:",                     value: "Std+EuroForth+Single+VAT",    complete: 712.80 },
-                { name: "Double room:",                     value: "Std+EuroForth+Double+VAT",    complete: 832.80 },
-            ]
-        },
-        {
-            header: "Forth standard meeting &  EuroForth conference (13.-17. September) without VAT",
-            description: "Choose this if you are a business in the EU and can provide a VAT-ID or if you are outside of the EU",
-            modes: [
-                { name: "Single room:",                     value: "Std+EuroForth+Single+NET",    complete: 594.00 },
-                { name: "Double room:",                     value: "Std+EuroForth+Double+NET",    complete: 694.00 },
+                { name: "Single room:",                     value: "Std+EuroForth+Single",    complete: 653.40 },
+                { name: "Double room:",                     value: "Std+EuroForth+Double",    complete: 763.40 },
             ]
         },
         {
             header: "EuroForth conference only (15.-17. September) with VAT",
-            description: "Choose this if you are a private traveller in the EU and cannot provide a VAT-ID, or if you live in Austria",
+            description: "Hotel with Breakfest, Meeting including Lunch",
             modes: [
-                { name: "Single room:",                     value: "EuroForth+Single+VAT",    complete: 420.00 },
-                { name: "Double room:",                     value: "EuroForth+Double+VAT",    complete: 480.00 },
-            ]
-        },
-        {
-            header: "EuroForth conference only (15.-17. September) without VAT",
-            description: "Choose this if you are a business in the EU and can provide a VAT-ID or if you are outside of the EU",
-            modes: [
-                { name: "Single room:",                     value: "EuroForth+Single+NET",    complete: 350.00 },
-                { name: "Double room:",                     value: "EuroForth+Double+NET",    complete: 400.00 },
+                { name: "Single room:",                     value: "EuroForth+Single+VAT",    complete: 385.00 },
+                { name: "Double room:",                     value: "EuroForth+Double+VAT",    complete: 440.00 },
             ]
         },
     ]
