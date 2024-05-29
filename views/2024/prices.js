4bcde7e16081a208086e9c55df351b509bea8b44
Gerald Wodni <gerald.wodni@gmail.com>
2024-05-29 14:41:29 +0200

    currency changed to GBP in email and success-message

--- views/2024/prices.js
+++ views/2024/prices.js
@@ -12,7 +12,7 @@ module.exports = {
     /* message to show after successful registration */
     successTemplate: function _successTemplate( { price } ) {
         return `Thank you for your registration!`
-            + `\nPlease transfer €${price} to the bank account provided in your email.`
+            + `\nPlease transfer £${price} to the bank account provided in your email.`
             //- + `\nIn the email you will find a personalized 'myEuroForth'-Link. Please bookmark it right away,`
             //- + `\nas this link allows you to participate electronically and to edit your presentation details.`
             + `\nYou will receive the email from: ${process.env.SMTP_EMAIL}.`;
@@ -20,7 +20,7 @@ module.exports = {
     /* registration email text body */
     emailTemplate: function _emailTemplate( { prices, values, price, website } ) {
         return `Hotel: ${values.hotel}\nExtra days: ${values.extraDays}\nPreis (total): ${price}`
-            + ( price > 0 ? `\nPlease transfer the full amount in Euro to:\n${process.env.OPERATOR_BANK_ACCOUNT.replace(/\\n/g, '\n')}` : '' )
+            + ( price > 0 ? `\nPlease transfer the full amount in GBP to:\n${process.env.OPERATOR_BANK_ACCOUNT.replace(/\\n/g, '\n')}` : '' )
             + ( prices.chatRegistration ? `\n\n${process.env.CHAT_REGISTRATION}` : '' )
             + `\n\nTo edit your presentation details, please use the following link: https://${website}/${prices.myName}/${values.editHash}`
             + `\n\nName: ${values.name}`
